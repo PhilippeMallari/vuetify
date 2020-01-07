63d9f7772f972cb52462af2b702fc7812b3987a6
philippe mallari <PhilippeMallari>
2020-01-07 19:51:34 +0800

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <Navbar />
 
-
-    <v-content>
+    <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
 
@@ -10,10 +10,12 @@
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar'
 
 
 export default {
   name: 'App',
+  components: {Navbar},
   data: () => ({
     //
   }),
---
+++ src/views/Team.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="team">
+<h1 class="subheading gray--text">Teams</h1>
+	<v-container class="my-5">
+		<v-layout row wrap>
+			<v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+				<v-card flat class="text-xs-center ma-3">
+					<v-responsive class="pt-4">
+						image
+					</v-responsive>
+					<v-card-text>
+						<div class="subheading">{{ person.name }}</div>
+						<div class="grey--text">{{ person.role }}</div>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn flat color="grey">
+							<v-icon small left>message</v-icon>
+							<span>Message</span>
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				team: [
+					{name: 'Philippe Mallari', role: 'Web Developer'},
+					{name: 'Ryu', role: 'Graphic Designer'},
+					{name: 'Chun Li', role: 'Web Developer'},
+					{name: 'Gouken', role: 'Social Media Maverick'},
+					{name: 'Yoshi', role: 'Sales Guru'}
+				]
+			}
+		}
+	}
+</script>
